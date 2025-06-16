d19ff5dd9bf59ccdd21d32d91fa0a30f6c0dd961
HDash <16350928+HDash@users.noreply.github.com>
2025-06-16 14:00:24 +0100

    Update EpiCompare run parameters
    Fixes issues with download buttons (now disabled)

--- 03_epicompare.R
+++ 03_epicompare.R
@@ -3,16 +3,18 @@ library(EpiCompare)
 # Setup
 data(hg38_blacklist)
 peakfiles <- list(
-    "CutTag_SK322_1" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_cuttag/5-shift_q0.01/SK322_1_R1_0.01_peaks.narrowPeak",
-    "CutTag_SK322_2" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_cuttag/5-shift_q0.01/SK322_2_R1_0.01_peaks.narrowPeak"
+    "CutTag_A303-531A_Tom" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_cuttag/5-shift_q0.01/SK342_8_R1_0.01_peaks.narrowPeak",
+    "CutTag_SAB4501855-100UG_Tom" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_cuttag/5-shift_q0.01/SK342_9_R1_0.01_peaks.narrowPeak",
+    "CutTag_A303-531A_Paulina" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_cuttag/Paulina_q1e-05/SK342_8_R1_1e-05_peaks.narrowPeak",
+    "CutTag_SAB4501855-100UG_Paulina" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_cuttag/Paulina_q1e-05/SK342_9_R1_1e-05_peaks.narrowPeak"
 )
 reference <- list(
-    "ChIP_ENCFF456USL" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_chipseq/encode/CEBPG_ENCFF456USL.bed"
+    "ChIP_ENCFF031NTF" = "/Users/hdash/code/cuttag_Isidora_TFprofiling/data/peaks_chipseq/encode/MEF2A_ENCFF031NTF.narrowPeak"
 )
 
 genome_build <- list(peakfiles="hg38", reference="hg38", blacklist="hg38")
 genome_build_output <- "hg38"
-out_dir <- "data/epicompare-reports/CEBPG"
+out_dir <- "data/epicompare-reports/MEF2A"
 cpu_cores <- 1 # Error if multi-core
 
 # Create directory
@@ -33,8 +35,17 @@ EpiCompare(
     genome_build_output = genome_build_output,
     output_dir = out_dir,
     debug = TRUE,
-    run_all = TRUE,
+    # run_all = TRUE,
+    upset_plot = TRUE,
+    stat_plot = TRUE,
+    chromHMM_plot = TRUE,
+    chipseeker_plot = TRUE,
+    enrichment_plot = TRUE,
+    tss_plot = TRUE,
+    precision_recall_plot = TRUE,
+    corr_plot = TRUE,
     add_download_button = FALSE,
-    workers = cpu_cores
+    workers = cpu_cores,
+    error = TRUE
 )
 
